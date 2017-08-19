b075174d9577dc1bd00ab811f6922d85e3942f6a
Kelly Smith <ksmith433@gatech.edu>
2017-08-18 22:36:59 -0500

    Implemented initial blind lane change

--- src/main.cpp
+++ src/main.cpp
@@ -424,6 +424,11 @@ int main() {
                 if (inFront && relRange < 30) {
                   //ref_vel = 29.5;
                   too_close = true;
+
+                  if (lane>0) {
+                    lane = 0;
+                  }
+
                 }
               }
             }
